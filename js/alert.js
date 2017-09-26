3922a58ce8a6190899c5ab5e9ee302cda22e1078
jiachenhao <s543355165@163.com>
2017-09-26 14:12:37 +0800

    commit

---
+++ js/alert.js
@@ -0,0 +1,18 @@
+// var green = 'green';
+// var red = 'red';
+// var blue = 'blue';
+// var aler = '.alert-css';
+//
+// function inval(value, bg) {
+//     this.value = value;
+//     this.bg = bg;
+//     $(aler).html(this.value);
+//     $(aler).addClass(this.bg);
+// }
+(function($) {
+    var alert = alert || {};
+    alert.property = function(txtHtml, type, options) {
+        var classType = alert.property.classType;
+
+    }
+})()
---
+++ js/mar.js
@@ -0,0 +1,5 @@
+var Mar = { //减小全局变量污染
+    init: function() {
+        alert(21);
+    }
+}
